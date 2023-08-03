8e4d556f1b160a0537edfa4e722d57f993cfe538
Daniel Silveira da Silva <danielsilveira51@gmail.com>
2023-08-03 13:51:33 -0300

    exe 011 012 013

---
+++ aula12/exe011.js
@@ -0,0 +1,10 @@
+var idade = 67
+if (idade < 16){
+    console.log('Não vota.')
+    } else if (idade >= 16 && idade < 18){
+        console.log('Voto opicional.{1}')
+    } else if (idade >= 67){
+        console.log('Voto opcional. {2}')
+    } else{
+        console.log('Voto obrigatório.')
+    }
