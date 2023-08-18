ac7261c6b27fdfa87480fb653dc1ae386a2bb21d
Daniel Silveira da Silva <danielsilveira51@gmail.com>
2023-08-18 12:29:53 -0300

    180823

--- desafios/exercícioanalisar/script.js
+++ desafios/exercícioanalisar/script.js
@@ -1,13 +1,14 @@
 var num = document.getElementById('ntxt');
 var valoresDigitados = [];
+var resultado = document.getElementById('res')
 
 function add() {
-    let numero = num.value; 
+    let numero = parseInt(num.value); 
     let tab = document.getElementById('analtab')
     
 
-    if (isNaN(numero) || numero >= 100 || num.value.length==0){
-        window.alert('errado')
+    if (isNaN(numero) || numero > 100 || num.value.length==0 || numero <= 0){
+        window.alert('Valor inválido ou inexistente, digite um número dentro do intervalo!')
     } else {
         valoresDigitados.push(numero);
         analtab.innerHTML = ""
@@ -17,4 +18,32 @@ function add() {
             tab.appendChild(item)
         }
     }
+}
+
+function end(){
+    let numero = parseInt(num.value); 
+    let maiorValor = valoresDigitados[0]; 
+    let menorValor = valoresDigitados[0]
+    let soma = ""
+    
+    if (valoresDigitados== -1){
+        valoresDigitados.push(numero)
+    }
+    
+    for (let i = 1; i < valoresDigitados.length; i++) {
+        if (valoresDigitados[i] > maiorValor) {
+            maiorValor = valoresDigitados[i];
+        }
+    }
+    for (let i = 1; i > valoresDigitados.length; i++){
+        if (valoresDigitados[i] < menorValor){
+            menorValor =  valoresDigitados[i]
+        }
+    }
+    for (let i = 1; valoresDigitados.length; i++){
+         soma += valoresDigitados[i]
+    }
+    resultado.innerHTML = `Ao todo temos ${valoresDigitados.length} números cadastrados.
+    </br> O maior valor é ${maiorValor}
+    </br> O menor valor é ${menorValor}`
 }
