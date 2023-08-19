6ea9e334bfb8fb529dca9c3beda96de55c83f212
Daniel Silveira da Silva <danielsilveira51@gmail.com>
2023-08-19 12:15:13 -0300

    190823

--- desafios/exercícioanalisar/script.js
+++ desafios/exercícioanalisar/script.js
@@ -7,9 +7,12 @@ function add() {
     let tab = document.getElementById('analtab')
     
 
-    if (isNaN(numero) || numero > 100 || num.value.length==0 || numero <= 0){
-        window.alert('Valor inválido ou inexistente, digite um número dentro do intervalo!')
-    } else {
+    if (isNaN(numero) || numero > 100 || num.value.length==0 || numero <= 0 || valoresDigitados.includes(numero)){
+        window.alert('Valor inválido ou inexistente, digite um número dentro do intervalo e que não esteja na lista!')
+    } else if (valoresDigitados== -1) {
+        window.alert('Este valor já está em sua lista.')
+    }
+    else {
         valoresDigitados.push(numero);
         analtab.innerHTML = ""
         for (var i = 0; i < valoresDigitados.length; i++){
@@ -24,7 +27,8 @@ function end(){
     let numero = parseInt(num.value); 
     let maiorValor = valoresDigitados[0]; 
     let menorValor = valoresDigitados[0]
-    let soma = ""
+    let soma = valoresDigitados[0]
+    let media = valoresDigitados[0]
     
     if (valoresDigitados== -1){
         valoresDigitados.push(numero)
@@ -40,10 +44,13 @@ function end(){
             menorValor =  valoresDigitados[i]
         }
     }
-    for (let i = 1; valoresDigitados.length; i++){
+    for (let i = 1; i < valoresDigitados.length; i++){
          soma += valoresDigitados[i]
+         media = soma/valoresDigitados.length
     }
     resultado.innerHTML = `Ao todo temos ${valoresDigitados.length} números cadastrados.
-    </br> O maior valor é ${maiorValor}
-    </br> O menor valor é ${menorValor}`
+    </br> O maior valor é ${maiorValor}.
+    </br> O menor valor é ${menorValor}.
+    </br> A soma dos valores é ${soma}.
+    </br> A média dos valores é ${media}.`
 }
